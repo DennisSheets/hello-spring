c1eef095bdf6ead3c938841c2e65488e8aaf8889
madsur <dennissheets@outlook.com>
2021-05-19 09:08:51 -0500

    updated to reasonably match Thymeleaf Part1 beginning code.

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -5,38 +5,38 @@ import org.springframework.web.bind.annotation.*;
 
 
 @Controller
-@ResponseBody
-@RequestMapping("hello")
+
+
 public class HelloController {
 
 
-//    @GetMapping("") // handles a get type request
-//    public String hello(){
-//        return "Hello Spring!";
-//    }
+
 
     // return a plain text response... until templates are learned
     @GetMapping("goodbye") // handles a get type request
+    @ResponseBody
     public String goodbye(){
         return "Goodbye Spring!";
     }
 
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name){
+    @RequestMapping(value="hello",method = {RequestMethod.GET, RequestMethod.POST})
+    public String hello(@RequestParam String name){
         return "Hello, " + name + "!";
     }
 
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name){
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloAgain(@PathVariable String name){
         return "Hello, " + name + "!";
     }
 
     @GetMapping("form")
+    @ResponseBody
     public String helloForm(){
         return "<html>" +
                     "<body>" +
-                        "<form action='/hello'>" +  //  method='post'
-                            "<input type='text' name='name'>" +
+                        "<form action='/hello' method='post>" +
+                            "<input type='text' name='gtname'>" +
                             "<input type='submit' value='Greet me!'>" +
                         "</form>" +
                     "</body>" +
